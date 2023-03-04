053f19f9adc11e43f433e0e05818a6a459ab5305
Sufyang737 <107362900+Sufyang737@users.noreply.github.com>
2023-03-04 12:29:06 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
 function test(){
 	var tabsNewAnim = $('#navbarSupportedContent');
 	var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
